42bafa4852fe511c079a5d8627f005bd589fbb14
loveltyoic <loveltyoic@gmail.com>
2013-07-15 20:18:38 +0800

    2018

--- app/controllers/search_infos_controller.rb
+++ app/controllers/search_infos_controller.rb
@@ -1,12 +1,12 @@
 class SearchInfosController < ApplicationController
   def result
-    @infos = Info.includes(:car).joins(:car).where("location like ? and rent_end > ? and status = 'active' ", "%" + params[:location] + "%", Time.now.to_date)
+    @infos = Info.includes(:car).where("cars.location like ? and rent_end > ? and status = 'active' ", "%" + params[:location] + "%", Time.now.to_date)
     @results = @infos.paginate(page: params[:page], per_page: 5)
     render 'result'
   end
 
   def search_date
-    @infos = Info.includes(:car).where("rent_start <= ? and rent_end >= ?", params[:rent_start], params[:rent_end])
+    @infos = @infos.where("rent_start <= ? and rent_end >= ?", params[:rent_start], params[:rent_end])
     @results = @infos.paginate(page: params[:page], per_page: 5)
     render 'result'
   end
