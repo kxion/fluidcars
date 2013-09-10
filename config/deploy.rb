a10dc33ebbdb747709e054bb6dc43d07a1a07dca
loveltyoic <loveltyoic@gmail.com>
2013-09-10 16:25:51 +0800

    remote_ip

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,6 +11,7 @@ set :scm, "git"
 set :port, "22"
 
 set :deploy_to, "/home/#{domain}"
+set :shared_path, "#{deploy_to}/shared"
 set :deploy_via, :remote_cache
 set :use_sudo, false
 
@@ -27,6 +28,11 @@ namespace :deploy do
   #   db_config = "#{shared_path}/database.yml"
   #   run "cp #{db_config} #{release_path}/config/database.yml"
   # end
+  namespace :assets do
+    task :precompile, :roles => :web, :except => { :no_release => true } do
+      logger.info "Skipping asset pre-compilation"
+    end
+  end
 
   desc "install the necessary prerequisites"
   task :bundle_install, :roles => :app do
@@ -45,13 +51,19 @@ namespace :deploy do
   task :restart, :roles => :app do
     run "kill -USR2 `cat #{deploy_to}/current/tmp/pids/unicorn.pid`"
   end
+
 end
 
 task :link_shared_files, :roles => :web do
   # run "ln -sf #{deploy_to}/shared/config/*.yml #{deploy_to}/current/config/"
   run "ln -sf #{deploy_to}/current/config/nginx.conf /etc/nginx/nginx.conf"
+  run "ln -sf #{deploy_to}/shared/assets #{deploy_to}/current/public/"
   # run "ln -sf #{deploy_to}/shared/config/unicorn.rb #{deploy_to}/current/config/"
 end
 
+task :compile_and_rsync_assets, roles => :web do 
+  run_locally "RAILS_ENV=production bundle exec rake assets:precompile; rsync -vr --exclude='.DS_Store' public/assets #{user}@#{domain}:#{shared_path}/"
+end
+
 # after "deploy:update_code", "deploy:copy_config_files" # 如果將database.yml放在shared下，請打開
-after "deploy:finalize_update", "deploy:bundle_install", :link_shared_files
+after "deploy:finalize_update", "deploy:bundle_install"#, :link_shared_files
