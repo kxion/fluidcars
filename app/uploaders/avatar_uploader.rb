d92b0746197a451d90a9f7af7cc910edd4fb9408
loveltyoic <loveltyoic@gmail.com>
2013-09-11 10:14:31 +0800

    修复上传问题，需要在Form头加入:html => {:multipart => true}

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -3,7 +3,9 @@ class AvatarUploader < ImageUploader
 
   def filename
     if super.present?
-      "avatar/#{file.extension.downcase}"
+      # current_path 是 Carrierwave 上传过程临时创建的一个文件，有时间标记，所以它将是唯一的
+      @name ||= Digest::MD5.hexdigest(File.dirname(current_path))
+      "#{@name}.#{file.extension.downcase}"
     end
   end
 
