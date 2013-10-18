e9c96a802be6720abd5938f78983bdf4a228ec23
loveltyoic <loveltyoic@gmail.com>
2013-10-18 16:49:28 +0800

    因为内存不足，将Unicorn worker变为4个

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -17,7 +17,7 @@ stdout_path "#{Rails.root}/log/unicorn.log"
 # listen 5000, :tcp_nopush => false
 
 listen "/tmp/unicorn.fluidcars.sock"
-worker_processes 10
+worker_processes 4
 timeout 120
 
 if GC.respond_to?(:copy_on_write_friendly=)
